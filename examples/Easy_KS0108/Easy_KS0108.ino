a15ed421713f93ce322638bc53abdd3583da56ec
easyrobot <semibank@naver.com>
2023-10-30 02:32:18 +0900

    신규
    신규

---
+++ examples/Easy_KS0108/Easy_KS0108.ino
@@ -0,0 +1,26 @@
+#include <EASY_KS0108.h>
+Adafruit_KS0108_kbv display;
+
+void setup(){
+  Serial.begin(115200) ;
+  display.begin();
+}
+
+void loop(){
+    display.clearDisplay() ;                 
+    EASY_putsxy(0,  0, "*스마트팜제어기*") ;
+    EASY_putsxy(0, 16, "1. 열림닫힘시간",WHITE) ; 
+    EASY_putsxy(0, 32, "2. 온도습도설정",BLACK,WHITE) ;
+    EASY_putsxy(0, 48, "3. 시간설정하기") ;
+    display.display() ;  
+    delay(2000);
+
+    display.clearDisplay() ;                 
+    EASY_putsxy(0,  0, "*습도설정메뉴*") ;
+    EASY_putsxy(0, 16, "1. 올림",WHITE) ; 
+    EASY_putsxy(0, 32, "2. 내림",BLACK,WHITE) ;
+    EASY_putsxy(0, 48, "3. 저장") ;
+    display.display() ;  
+    delay(2000);
+}
+
---
+++ examples/Easy_KS0108_ascii/Easy_KS0108_ascii.ino
@@ -0,0 +1,50 @@
+#include <EASY_KS0108.h>
+Adafruit_KS0108_kbv display;
+
+void setup(){
+  Serial.begin(115200) ;
+  display.begin();
+}
+
+void loop(){
+    display.clearDisplay() ;     
+    /*            
+    EasyPrint(0,  0, "*스마트팜제어기*") ;
+    EasyPrint(0, 16, "1. 열림닫힘시간",WHITE) ; 
+    EasyWrite(0, 32, char(41),BLACK,WHITE) ;
+    EasyWrite(0, 0, char(20)) ;
+    */
+    display.clearDisplay() ;  
+    EASY_setxy(0,0);
+    for(int i=0;i<64;i++){
+        EASY_putc(char(i));
+    }    
+    display.display() ;  
+    delay(2000);
+    
+    display.clearDisplay() ;  
+    EASY_setxy(0,0);
+    for(int i=64;i<128;i++){
+        EASY_putc(char(i));
+    }
+    display.display() ;  
+    delay(2000);
+    
+    
+    display.clearDisplay() ;  
+    display.setCursor(0, 0);  
+    for(int i=0;i<128;i++){
+        display.write(char(i));
+    }    
+    display.display() ;  
+    delay(2000);
+    display.clearDisplay() ;  
+    display.setCursor(0, 0);  
+    for(int i=128;i<256;i++){
+        display.write(char(i));
+    }
+    display.display() ;  
+    delay(2000);
+   
+}
+
